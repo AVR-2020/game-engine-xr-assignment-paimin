e1344a7e58620cc996db2ef9c9ffc7fa136854ff
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-01-07 14:10:39 +0700

    Create Main Menu

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void QuitTogame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
