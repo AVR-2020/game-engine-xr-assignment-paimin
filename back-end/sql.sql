655ab7b329aba2b798dca2b180aa3c55bfa5a805
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-01-08 17:34:40 +0700

    Create sql.sql

---
+++ back-end/sql.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Community v13.1.5  (64 bit)
+MySQL - 10.3.16-MariaDB : Database - geweher
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`geweher` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `geweher`;
+
+/*Table structure for table `scores` */
+
+DROP TABLE IF EXISTS `scores`;
+
+CREATE TABLE `scores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `value_score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `scores` */
+
+LOCK TABLES `scores` WRITE;
+
+insert  into `scores`(`id`,`name`,`value_score`) values 
+(1,'fadly',1230),
+(2,'jh',1230),
+(3,'asdf',123);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
