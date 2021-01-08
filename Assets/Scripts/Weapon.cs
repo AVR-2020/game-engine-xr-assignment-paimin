f8d781df9b1ba6f6c0f535909e526ec39eb86f3a
irshadrasyidi <36858141+irshadrasyidi@users.noreply.github.com>
2021-01-08 18:43:06 +0700

    add shoot mechanic + bullet

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,18 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(XRGrabInteractable))]
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected float shootingForce;
+    [SerializeField] protected Transform bulletSpawn;
+    [SerializeField] private float recoilForce;
+    [SerializeField] private float damage;
+
+    private Rigidbody rigidBody;
+    private XRGrabInteractable interactableWeapon;
+
+    protected virtual void Awake()
+    {
+        interactableWeapon = GetComponent<XRGrabInteractable>();
+        rigidBody = GetComponent<Rigidbody>();
+        SetupInteractableWeaponEvents();
+    }
+
+    private void SetupInteractableWeaponEvents()
+    {
+        interactableWeapon.onSelectEnter.AddListener(PickUpWeapon);
+        interactableWeapon.onSelectExit.AddListener(DropWeapon);
+        interactableWeapon.onActivate.AddListener(StartShooting);
+        interactableWeapon.onDeactivate.AddListener(StopShooting);
+    }
+
+    private void PickUpWeapon(XRBaseInteractor interactor)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DropWeapon(XRBaseInteractor interactor)
     {
         
     }
+
+
+    protected virtual void StartShooting(XRBaseInteractor interactor)
+    {
+        
+    }
+
+    protected virtual void StopShooting(XRBaseInteractor interactor)
+    {
+
+    }
+
+    protected virtual void Shoot()
+    {
+        ApplyRecoil();
+    }
+
+    private void ApplyRecoil()
+    {
+        rigidBody.AddRelativeForce(Vector3.back * recoilForce, ForceMode.Impulse);
+    }
+
+    public float GetShootingForce()
+    {
+        return shootingForce;
+    }
+
+    public float GetDamage()
+    {
+        return damage;
+    }
+
 }
