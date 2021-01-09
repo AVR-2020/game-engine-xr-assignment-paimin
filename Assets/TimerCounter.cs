ed55c37f7542c53924eec6dc35f53c2c4e3c3153
Nadhif Haikal Farhansyah <neorocket64@gmail.com>
2021-01-09 21:31:23 +0700

    Added time counter

--- Assets/TimerCounter.cs
+++ Assets/TimerCounter.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerCounter : MonoBehaviour
 {
+    Text text;
+    private static float TimerValue = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<Text> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Debug.Log(this.GetTimerValue());
+        // text.text = System.Convert.ToString(System.Math.Round(GetTimerValue(), 2));
+        text.text = GetTimerValue().ToString("0.00");
+        AddTime();
     }
+    public void SetTime(float second)
+    {
+        TimerValue = second;
+    }
+    public void AddTime()
+    {
+        TimerValue += Time.deltaTime;
+    }
+    public double GetTimerValue()
+    {
+        return System.Math.Round(TimerValue, 2);
+    }
+
+
 }
