4251755f4c345125d4efe0119512a72f0f5ffc49
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-01-09 12:26:25 +0700

    Add one hit dead npc and overlay for score and time

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    private int ScoreValue = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.GetScoreValue());
+    }
+    public void AddScore(int point)
+    {
+        this.ScoreValue += point;
+    }
+    public int GetScoreValue ()
+    {
+        return this.ScoreValue;
+    }
+}
