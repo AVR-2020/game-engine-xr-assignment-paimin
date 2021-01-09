98fd7bcfe96076469841acdaf3ba0285af953f56
om magang <om.magang@jagoanhosting.com>
2021-01-09 11:12:45 +0700

    add testing enemy scene, enemy ai script, and player script

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,18 +1,122 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAI : MonoBehaviour, ITakeDamage
 {
-    // Start is called before the first frame update
-    void Start()
+    const string RUN_TRIGGER = "Run";
+    const string CROUCH_TRIGGER = "Crouch";
+    const string SHOOT_TRIGGER = "Shoot";
+
+    [SerializeField] private float startingHealth, minTimeUnderCover, maxTimeUnderCover, rotationSpeed, damage;
+    [SerializeField] private int minShotsToTake, maxShotsToTake;
+    [Range(0, 100)]
+    [SerializeField] private float shootingAccuracy;
+
+    [SerializeField] private Transform shootingPosition;
+
+    private bool isShooting;
+    private int currentShotsTaken;
+    private int currentMaxShotsToTake;
+    private NavMeshAgent agent;
+    private Transform player;
+    private Transform occupiedCoverSpot;
+    private Animator animator;
+
+    private float _health;
+
+    public float health
     {
-        
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = Mathf.Clamp(value, 0, startingHealth);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        animator.SetTrigger(RUN_TRIGGER);
+        _health = startingHealth;
+    }
+
+    public void Init(Transform player, Transform coverSpot)
+    {
+        occupiedCoverSpot = coverSpot;
+        this.player = player;
+        GetToCover();
+    }
+
+    private void GetToCover()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(occupiedCoverSpot.position);
+    }
+
+    private void Update()
+    {
+        if(agent.isStopped == false && (transform.position - occupiedCoverSpot.position).sqrMagnitude <= 0.1f)
+        {
+            agent.isStopped = true;
+            StartCoroutine(InitializeShootingCO());
+        }
+        if(isShooting)
+        {
+            RotateTowardsPlayer();
+        }
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        throw new NotImplementedException();
+    }
+
+    private IEnumerator InitializeShootingCO()
+    {
+        HideBehindCover();
+        yield return new WaitForSeconds(UnityEngine.Random.Range(minTimeUnderCover, maxTimeUnderCover));
+        StartShooting();
+    }
+
+    private void StartShooting()
+    {
+        isShooting = true;
+        currentMaxShotsToTake = UnityEngine.Random.Range(minShotsToTake, maxShotsToTake);
+        currentShotsTaken = 0;
+        animator.SetTrigger(SHOOT_TRIGGER);
+    }
+
+    public void Shoot()
+    {
+        bool hitPlayer = UnityEngine.Random.Range(0, 100) < shootingAccuracy;
         
+        if (hitPlayer)
+        {
+            RaycastHit hit;
+            Vector3 direction = player.position - shootingPosition.position;
+            if(Physics.Raycast(shootingPosition.position, direction, out hit))
+            {
+
+            }
+        }
+    }
+
+    private void HideBehindCover()
+    {
+        animator.SetTrigger(CROUCH_TRIGGER);
+    }
+
+    public void TakeDamage(Weapon weapon, Projectile projectile, Vector3 contactPoint)
+    {
+        throw new System.NotImplementedException();
     }
 }
