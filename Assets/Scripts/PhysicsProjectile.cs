21ef43d5c783b562ad38fd08bf3cd0536120cb0f
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2021-01-09 08:11:26 +0700

    add rifle

--- Assets/Scripts/PhysicsProjectile.cs
+++ Assets/Scripts/PhysicsProjectile.cs
@@ -26,4 +26,15 @@ public class PhysicsProjectile : Projectile
         rigidBody.AddRelativeForce(Vector3.forward * weapon.GetShootingForce(), ForceMode.Impulse);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        ITakeDamage[] damageTakers = other.GetComponentsInChildren<ITakeDamage>();
+
+        foreach (var taker in damageTakers)
+        {
+            taker.TakeDamage(weapon, this, transform.position);
+        }
+    }
+
 }
