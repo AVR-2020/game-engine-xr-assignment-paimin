6cd242cf1143a61d7c7a1dcfe7fadc48360023bd
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-01-10 11:06:43 +0700

    Add Keyboard Input

--- Assets/Scripts/KeyboardInput.cs
+++ Assets/Scripts/KeyboardInput.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyboardInput : MonoBehaviour
 {
+    public GameObject txt;
     // Start is called before the first frame update
     void Start()
     {
+        txt = GameObject.Find("NameText");
     }
     // Update is called once per frame
     void Update()
     {
     }
     public void GetText() {
-        Debug.Log(this.name);
+        
+        txt.GetComponent<TextMeshPro>().text += this.name;
+        //txt.GetComponent().text = this.name;
+
     }
 }
